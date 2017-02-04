829cb3f159f34836087337043e4f01ce1de6c6e1
Adam Balint <ab11fl@brocku.ca>
2017-02-03 22:07:41 -0500

    Removed 500MB matrix file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import sys
 sys.path.insert(0, "AI")
 import train_style as ts
+import compare_net as cn
+from scipy.misc import imread
 
-ts.train(0,1,2,3)
+#ts.train(0,1,2,3)
+
+img = imread("../test.jpg")
+
+d = cn.get_style_features(img)
+print(d)
