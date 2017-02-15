f6d79130e6f8e2ad0414e97f807c0f73080fe973
fzxt <fahadahmad41@hotmail.com>
2017-02-15 12:52:36 -0500

    Changed `frontend/styles` to `frontend/global_styles`
    - Updated components and stylesheets to match directory changes

--- src/UI/frontend/app/util/Icons.js
+++ src/UI/frontend/app/util/Icons.js
@@ -2,6 +2,6 @@
  * Contains various constants for icons
  */
 
-export const irisLogo = require('../../sassets/img/iris-logo.svg');
+export const irisLogo = require('../../assets/img/iris-logo.svg');
 export const cameraIcon = require('../../assets/img/filepicker-camera.svg');
 export const cartIcon = require('../../assets/img/cart-icon.svg');
